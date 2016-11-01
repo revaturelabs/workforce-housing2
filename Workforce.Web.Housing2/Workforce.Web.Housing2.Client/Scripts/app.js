8efc1f0d3508fc3d3b452dea26211f8eff0dae10
SmokedEel <msdilinila@gmail.com>
2016-11-01 14:53:30 -0400

    Issue/migrate crud web routing (#4)
    * Added angular and bootstrap references.  Created a javascript file to do routing between pages.
    
    * Routing added.  Started work on complexes module.

---
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
@@ -0,0 +1,15 @@
+﻿
+(function (ga) {
+    'use strict';
+
+    ga.angular = angular.module('gtApt', ['ngRoute', 'gtComplex']);
+    ga.angular.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            controller: 'complexController',
+            templateUrl: 'modules/complexes/view.html'
+        })
+            
+        .otherwise({ redirectTo: '/' })
+    }]);
+
+})(window.gtApp || (window.gtApp = {}));
---
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Complexes/module.js
@@ -0,0 +1,11 @@
+﻿(function (ga) {
+    'use strict';
+
+    ga.complex = angular.module('gtComplex', []);
+
+    ga.complex.controller('complexController', ['$scope', function ($scope) {
+   
+        $scope.test = 'hi there';
+    }])
+
+})(window.gtApp);
