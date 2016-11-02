da424182c52efe9d41336cc485597e7e5d786b90
brandon.parnoff <brandon.parnoff@outlook.com>
2016-11-02 16:00:10 -0400

    added controllers

---
+++ Workforce.Logic.Housing2/Workforce.Logic.Housing2.Rest/Controllers/ApartmentController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Workforce.Logic.Housing2.Rest.Controllers
+{
+    public class ApartmentController : ApiController
+    {
+    }
+}
