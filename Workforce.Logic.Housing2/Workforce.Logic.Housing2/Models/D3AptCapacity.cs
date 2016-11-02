983161e30fe4a2ca45507aec1a3fb6e8b5c70f70
brandon.parnoff <brandon.parnoff@outlook.com>
2016-11-02 15:21:55 -0400

    Added and populated Models

---
+++ Workforce.Logic.Housing2/Workforce.Logic.Housing2/Models/D3AptCapacity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workforce.Logic.Housing2.Models
+{
+   public class D3AptCapacity
+   {
+
+      /// <summary>
+      /// this method makes the JSON/XML model for the d3JS graph to consume
+      /// </summary>
+      /// <returns>Task<List<GraphAptCapacityDto>></returns>
+      public async Task<List<GraphAptCapacityDto>> getNewModel()
+      {
+         HousingComplex mapper = new HousingComplex();
+         List<GraphAptCapacityDto> returnGraph = new List<GraphAptCapacityDto>();
+         foreach (var item in await graceService.GetComplexesAsync())
+         {
+            if (item.ActiveBit)
+            {
+               returnGraph.Add(
+                 new GraphAptCapacityDto()
+                 {
+                    name = item.Name,
+                    maxCapacity = await returnComplexMaxCap(mapper.MapToDto(item)),
+                    currentCapacity = await returnComplexCurCap(mapper.MapToDto(item))
+
+                 });
+            }
+         }
+         return returnGraph;
+      }
+
+      private readonly GraceServiceClient graceService = new GraceServiceClient();
+
+      /// <summary>
+      /// method to give the max capacity of the given apartment 
+      /// </summary>
+      /// <param name="hotApt"></param>
+      /// <returns>Task<int></returns>
+      public async Task<int> returnComplexCurCap(HousingComplexDto hotApt)
+      {
+         int Total = 0;
+         foreach (var item in await graceService.GetApartmentsAsync())
+         {
+            if (item.ActiveBit && (item.HotelID == hotApt.HotelID))
+            {
+               Total += item.CurrentCapacity;
+            }
+         }
+         return Total;
+      }
+
+      /// <summary>
+      /// method to give the current capacity of the given apartment complex
+      /// </summary>
+      /// <param name="hotApt"></param>
+      /// <returns>Task<int></returns>
+      public async Task<int> returnComplexMaxCap(HousingComplexDto hotApt)
+      {
+         int Total = 0;
+         foreach (var item in await graceService.GetApartmentsAsync())
+         {
+            if (item.ActiveBit && (item.HotelID == hotApt.HotelID))
+            {
+               Total += item.MaxCapacity;
+            }
+         }
+         return Total;
+      }
+
+   }
+}
---
+++ Workforce.Logic.Housing2/Workforce.Logic.Housing2/Models/D3DateProjection.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workforce.Logic.Housing2.Helpers;
+
+namespace Workforce.Logic.Housing2.Models
+{
+   public class D3DateProjection
+   {
+      private readonly GraceServiceClient graceService = new GraceServiceClient();
+      private readonly LogicHelper logicHelper = new LogicHelper();
+
+      private readonly Consumers consumerHelper = new Consumers();
+
+      /// <summary>
+      /// this method returns the model needed for d3js projections
+      /// </summary>
+      /// <param name="projectionDate"></param>
+      /// <returns></returns>
+      public async Task<List<D3Projection>> getNewModel(DateTime projectionDate)
+      {
+         int totalMaxCapacity = 0;
+         int totalCurCapacity = 2;
+
+         foreach (var item in await logicHelper.HousingComplexsGetActive())
+         {
+            totalMaxCapacity += await returnComplexMaxCap(item);
+            //totalCurCapacity += await returnComplexCurCap(item);
+         }
+
+         List<D3Projection> returnList = new List<D3Projection>();
+         for (int i = 0; i < 37; i++)
+         {
+            D3Projection temp = new D3Projection();
+            temp.TotalMax = totalMaxCapacity;
+            temp.Date = projectionDate.ToString("yyyy-MM-dd");
+            //temp.CurrentCapacity = totalCurCapacity;
+            //totalCurCapacity += 3;
+            temp.CurrentCapacity = await findCurrCapDate(projectionDate);
+            returnList.Add(temp);
+            projectionDate = projectionDate.AddDays(1.0);
+         }
+
+
+         return returnList;
+      }
+
+      /// <summary>
+      /// method to get the current capacity of a given date
+      /// </summary>
+      /// <param name="dateTime"></param>
+      /// <returns></returns>
+      private async Task<int> findCurrCapDate(DateTime dateTime)
+      {
+         int value = 0;
+         //List<AssociateDto> assoc = await consumerHelper.ConsumeAssociatesFromAPI();
+         //FIND THE APARTMENT
+
+         List<HousingDataDto> dataList = await logicHelper.HousingDataGetAll();
+
+         List<HousingDataDto> returnList = new List<HousingDataDto>();
+
+         foreach (var item in dataList)
+         {
+            int b1 = (item.MoveInDate.CompareTo(dateTime));
+            int b2 = (dateTime.CompareTo(item.MoveOutDate));
+
+            if ((b1 == -1 || b1 == 0) && (b2 == 0 || b2 == 1))
+            {
+               returnList.Add(item);
+            }
+
+         }
+
+         List<ApartmentDto> aptDto = await logicHelper.ApartmentsGetAll();
+
+         foreach (var item in returnList)
+         {
+            if (item.RoomID != null)
+            {
+               ApartmentDto temp = aptDto.Find(id => id.RoomID.Equals(item.RoomID));
+               value += temp.CurrentCapacity;
+            }
+         }
+
+
+         return value;
+
+
+      }
+      /// <summary>
+      /// this method returns the int value of the current capacity for the given complex
+      /// </summary>
+      /// <param name="hotApt"></param>
+      /// <returns></returns>
+      private async Task<int> returnComplexCurCap(HousingComplexDto hotApt)
+      {
+         int Total = 0;
+         foreach (var item in await graceService.GetApartmentsAsync())
+         {
+            if (item.ActiveBit && (item.HotelID == hotApt.HotelID))
+            {
+               Total += item.CurrentCapacity;
+            }
+         }
+         return Total;
+      }
+
+      /// <summary>
+      /// this method returns the int value of the max capacity for the given complex
+      /// </summary>
+      /// <param name="hotApt"></param>
+      /// <returns></returns>
+      private async Task<int> returnComplexMaxCap(HousingComplexDto hotApt)
+      {
+         int Total = 0;
+         foreach (var item in await graceService.GetApartmentsAsync())
+         {
+            if (item.ActiveBit && (item.HotelID == hotApt.HotelID))
+            {
+               Total += item.MaxCapacity;
+            }
+         }
+         return Total;
+      }
+   }
+}
