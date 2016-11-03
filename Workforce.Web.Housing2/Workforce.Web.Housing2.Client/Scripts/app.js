60f3f6e13718d385817a389eb6f4938118285ebd
SmokedEel <marc.dilinila@gmail.com>
2016-11-03 15:36:49 -0400

    Forgot to add associate module to angular module.

--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
@@ -1,7 +1,7 @@
 ﻿(function (ga) {
   'use strict';
 
-  ga.angular = angular.module('ahApp', ['ngRoute', 'ahComplex']);
+  ga.angular = angular.module('ahApp', ['ngRoute', 'ahComplex', 'ahAssociate']);
   ga.angular.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
       controller: 'complexController',
