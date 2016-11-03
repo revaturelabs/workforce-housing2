bb4741e02fde42d42cce302a323fc5d0c0d8c41e
SmokedEel <marc.dilinila@gmail.com>
2016-11-03 15:19:01 -0400

    Addition of associate module.  Will not add much more since I don't want to change app.js before a merge

---
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Associates/module.js
@@ -0,0 +1,12 @@
+﻿(function (ga) {
+    'use strict';
+
+    ga.associate = angular.module('gtAssociate', []);
+
+    ga.associate.controller('associateController', ['$scope', function ($scope) {
+                      
+        $scope.test = 'Hi There';
+
+    }]);
+
+})(window.gtApp);
