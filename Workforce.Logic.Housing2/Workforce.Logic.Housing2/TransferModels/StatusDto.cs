0dd50fcf99958d3b4325a539560c3dd98a82e159
Brandon Parnoff <brandon.parnoff@outlook.com>
2016-11-03 14:33:35 -0400

    Story/migrate-grace-logic (#7)
    * Created Logic Layer
    
    * Added Helpers
    
    * Populated Helpers
    
    * Added and populated Models
    
    * Added and populated Transfer Models
    
    * Corrected Naming issues; Added references
    
    * added controllers
    
    * Populated Controllers
    
    * removed all code-dependent code
    
    * hunted last broken reference

---
+++ Workforce.Logic.Housing2/Workforce.Logic.Housing2/TransferModels/StatusDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workforce.Logic.Housing2.TransferModels
+{
+   public class StatusDto
+   {
+      public int StatusID { get; set; }
+      public string StatusColor { get; set; }
+      public string StatusMessage { get; set; }
+      public bool ActiveBit { get; set; }
+   }
+}
