051732f9cea65a36519bfb8c9afa57ae774d70a2
daniel <danielsmcelhaney@gmail.com>
2016-11-04 14:18:11 -0400

    add back in apartments folder

--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
@@ -1,16 +1,16 @@
 ﻿(function (ga) {
   'use strict';
 
-  ga.angular = angular.module('ahApp', ['ngRoute', 'ahComplex', /*'ahApartment',*/ 'ahAssociate']);
+  ga.angular = angular.module('ahApp', ['ngRoute', 'ahComplex', 'ahApartment', 'ahAssociate']);
   ga.angular.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
       controller: 'complexController',
       templateUrl: 'modules/Complexes/view.html'
     })
-        //.when('/apartment', {
-        //    controller: 'apartmentController',
-        //    templateUrl: 'modules/apartment/view.html'
-        //})
+        .when('/apartments', {
+            controller: 'apartmentController',
+            templateUrl: 'modules/apartment/view.html'
+        })
         .when('/rooming', {
             controller: 'associateController',
             templateUrl: 'modules/Associates/view.html'
