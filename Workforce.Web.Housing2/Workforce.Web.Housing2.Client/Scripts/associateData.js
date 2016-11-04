3fd8245ad0e249275307dbd0d06f0eba49824d8f
SmokedEel <marc.dilinila@gmail.com>
2016-11-04 19:08:00 -0400

    Added the ability to obtain information about a specific associate

---
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/associateData.js
@@ -0,0 +1,22 @@
+﻿(function (ga) {
+    'use strict';
+
+    ga.angular.factory('traineeDataGrab', [function () {
+
+
+        function set(data) {
+            sessionStorage.setItem('aTrainee', JSON.stringify(data)); //stringified.  needs to be parse       
+        }
+
+        function get() {
+            var x = JSON.parse(sessionStorage.getItem('aTrainee')); //using sessionstorage
+            return x;
+        }
+
+        return {
+
+            set: set,
+            get: get
+        };
+    }]);
+})(window.ahApp);
