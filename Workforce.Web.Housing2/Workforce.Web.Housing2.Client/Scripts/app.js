e361d7414c3781f7b8df1d8db8c8456884a0ee75
SmokedEel <marc.dilinila@gmail.com>
2016-11-04 11:27:19 -0400

    Added addition of associates service.  Added apartment routing to app.js

--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
@@ -1,12 +1,16 @@
 ﻿(function (ga) {
   'use strict';
 
-  ga.angular = angular.module('ahApp', ['ngRoute', 'ahComplex', 'ahAssociate']);
+  ga.angular = angular.module('ahApp', ['ngRoute', 'ahComplex', 'ahApartment', 'ahAssociate']);
   ga.angular.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
       controller: 'complexController',
       templateUrl: 'modules/Complexes/view.html'
     })
+        .when('/apartment', {
+            controller: 'apartmentController',
+            templateUrl: 'modules/apartment/view.html'
+        })
         .when('/rooming', {
             controller: 'associateController',
             templateUrl: 'modules/Associates/view.html'
--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Associates/service.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Associates/service.js
@@ -1,1 +1,60 @@
 ﻿
+(function (ga) {
+    'use strict';
+
+    ga.associate.factory('traineeGetService', ['$http', function ($http) {
+        var url = '/workforce-grace-rest/api/associate/';
+        return {
+            get: function (data, pass, fail) {
+                $http({
+                    method: 'get',
+                    url: url,
+                    params: data
+                }).then(pass, fail);
+            }
+        }
+    }]);
+
+    ga.associate.factory('associateByAptService', ['$http', function ($http) {
+        var url = '/workforce-grace-rest/api/associate/';
+        return {
+            get: function (data, pass, fail) {
+                $http({
+                    method: 'get',
+                    url: url,
+                    params: data
+                }).then(pass, fail);
+            }
+        }
+    }]);
+
+    ga.associate.factory('associatePostService', ['$http', function ($http) {
+        var url = '/workforce-grace-rest/api/associate/';
+        return {
+            addAssoc: function (info, pass, fail) {
+                $http({
+                    method: 'post',
+                    url: url,
+                    data: info
+                }).then(pass, fail);
+            }
+        }
+    }]);
+
+    ga.associate.factory('associateDeleteService', ['$http', function ($http) {
+        var url = '/workforce-grace-rest/api/associate/';
+        return {
+            removeAssoc: function (info, pass, fail) {
+                $http({
+                    method: 'delete',
+                    url: url,
+                    data: info,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(pass, fail);
+            }
+        }
+    }]);
+
+})(window.ahApp);
