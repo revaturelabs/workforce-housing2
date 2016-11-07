f7ef9e647e94255ea5d42fb5d3eb8706505aa897
7-- <regop412@gmail.com>
2016-11-06 22:32:03 -0500

    logic domain project. will work on rest project

---
+++ Workforce.Logic.Housing2/Workforce.Logic.Domain/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workforce.Logic.Domain.Models
+{
+  class Class1
+  {
+  }
+}
---
+++ Workforce.Logic.Housing2/Workforce.Logic.Domain/TransferModels/Dtos/AssociateDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workforce.Logic.Domain.TransferModels.Dtos
+{
+  public class AssociateDto
+  {
+    public int AssociateID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Gender { get; set; }  
+    public int BatchID { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public bool HasCar { get; set; }
+    public bool HasKeys { get; set; }
+    public bool? IsComing { get; set; }
+  }
+}
