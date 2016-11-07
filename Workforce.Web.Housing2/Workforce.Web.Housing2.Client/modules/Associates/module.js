a477f99c5e2b72f8f8c5d3147075d15c2731b6b9
SmokedEel <marc.dilinila@gmail.com>
2016-11-07 10:45:30 -0500

    Added pagination to associates module.

--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Associates/module.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Associates/module.js
@@ -5,8 +5,25 @@
 
     ga.associate.controller('associateController', ['$scope', function ($scope) {
                       
-        $scope.test = 'Hi There';
+        $scope.filteredTrainees = [];
+        $scope.currentPage = 1;
+        $scope.numPerPage = 5;
+        $scope.traineeFirstName = 'Click an Associate';
 
+        $scope.setPage = function (pageNo) {
+            $scope.currentPage = pageNo;
+        }
+
+        $scope.back = function () {
+            $window.location.href = '#/apartments';
+        }
+
+        $scope.pageChanged = function () {
+            var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+            , end = begin + $scope.numPerPage;
+
+            $scope.filteredTrainees = $scope.trainees.slice(begin, end);
+        };
     }]);
 
 })(window.ahApp);
