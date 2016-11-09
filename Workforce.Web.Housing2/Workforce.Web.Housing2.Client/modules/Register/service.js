7732dd03a4292b4d17518386ebddbcc5c13c162a
danielsmcelhaney <danielsmcelhaney@gmail.com>
2016-11-09 16:08:29 -0500

    add login, register modules TG-40 #in-progress (#16)
    * add login, register modules TG-40 #in-progress
    
    * add login form and login module, logincontroller TG-40
    
    * add register view, module TG-40
    
    * make gender radio buttons look better TG-40
    
    * add other to gender options TG-40
    
    * add login authenticate code to module, service; add registration register code to module, service TG-40
    
    * make sonarqube happy TG-40
    
    * update routing, change gender from other to not specific TG-40

---
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Register/service.js
@@ -0,0 +1,13 @@
+﻿(function (ga) {
+  'use strict';
+
+  ga.register.factory('registerService', ['$http', function ($http) {
+    return {
+      get: function () {
+        $http({
+
+        })
+      }
+    }
+  }]);
+})(window.ahApp);
