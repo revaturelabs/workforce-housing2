7f995e64b9ac2c9d99fc4cd6166e03a9b5fb7c0f
daniels <danielsmcelhaney@gmail.com>
2016-11-09 10:17:16 -0500

    add register view, module TG-40

--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
@@ -1,7 +1,7 @@
 ﻿(function (ga) {
   'use strict';
 
-  ga.angular = angular.module('ahApp', ['ngRoute', 'ahComplex', 'ahApartment', 'ahAssociate', 'ahLogin']);
+  ga.angular = angular.module('ahApp', ['ngRoute', 'ahComplex', 'ahApartment', 'ahAssociate', 'ahLogin', 'ahRegister']);
   ga.angular.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
       controller: 'complexController',
@@ -18,6 +18,10 @@
       .when('/login', {
         controller: 'loginController',
         templateUrl: 'modules/Login/view.html'
+      })
+      .when('/register', {
+        controller: 'registerController',
+        templateUrl: 'modules/Register/view.html'
       })
     .otherwise({ redirectTo: '/' })
   }]);
--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Register/module.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Register/module.js
@@ -1,1 +1,7 @@
-﻿
+﻿(function (ga) {
+  ga.register = angular.module('ahRegister', []);
+
+  ga.register.controller('registerController', ['$scope', function ($scope) {
+
+  }]);
+})(window.ahApp);
