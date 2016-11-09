f343f4dfa633adb2c23030d6de342971c6425b4b
daniels <danielsmcelhaney@gmail.com>
2016-11-09 15:07:28 -0500

    update routing, change gender from other to not specific TG-40

--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/Scripts/app.js
@@ -1,20 +1,32 @@
 ﻿(function (ga) {
   'use strict';
 
-  ga.angular = angular.module('ahApp', ['ngRoute', 'ngMessages', 'ahComplex', 'ahApartment', 'ahAssociate']);
+  ga.angular = angular.module('ahApp', ['ngRoute', 'ngMessages', 'ahComplex', 'ahApartment', 'ahAssociate', 'ahLogin', 'ahRegister']);
   ga.angular.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
-      controller: 'complexController',
-      templateUrl: 'modules/Complexes/view.html'
+      controller: 'loginController',
+      templateUrl: 'modules/Login/view.html'
     })
         .when('/apartments', {
             controller: 'apartmentController',
             templateUrl: 'modules/Apartments/view.html'
         })
+      .when('/complexes', {
+        controller: 'complexController',
+        templateUrl: 'modules/Complexes/view.html'
+      })
         .when('/rooming', {
             controller: 'associateController',
             templateUrl: 'modules/Associates/view.html'
         })
+      .when('/login', {
+        controller: 'loginController',
+        templateUrl: 'modules/Login/view.html'
+      })
+      .when('/register', {
+        controller: 'registerController',
+        templateUrl: 'modules/Register/view.html'
+      })
     .otherwise({ redirectTo: '/' })
   }]);
 
