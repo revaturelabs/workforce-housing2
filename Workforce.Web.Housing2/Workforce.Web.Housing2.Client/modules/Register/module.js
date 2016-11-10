b1d29556f253a6858fce8ebe9e27be7a35d8241f
Sam Choo <samchoo@outlook.com>
2016-11-10 16:19:57 -0500

    Add login check for modules/Register/module.js

--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Register/module.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Register/module.js
@@ -2,14 +2,19 @@
   ga.register = angular.module('ahRegister', []);
 
   ga.register.controller('registerController', ['$scope', 'registerService', function ($scope, registerService) {
+    function check() {
+      var sessionItem = sessionStorage.getItem('Login');
+      if (sessionItem != "true") {
+        location = '#/login';
+      }
+    }
+
     $scope.register = function (fname, lname, uEmail, male, female, notSpec) {
       var u = { Email: uEmail, FirstName: fname, LastName: lname };
-      if (male === true)
-      {
+      if (male === true) {
         u.Gender = male;
       }
-      else
-      {
+      else {
         u.Gender = female;
       }
       registerService.post(u, function () { location = '#/thanksForRegister'; }, function () { location = '#/fail'; });
