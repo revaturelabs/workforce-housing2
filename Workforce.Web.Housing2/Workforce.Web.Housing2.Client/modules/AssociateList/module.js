6abbd3d0d4462b6d42faf44d600b08f757d34a70
SmokedEel <marc.dilinila@gmail.com>
2016-11-10 16:21:12 -0500

    Initial commit, to start off the pull request

---
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/AssociateList/module.js
@@ -0,0 +1,11 @@
+﻿
+(function (ga) {
+    'use strict';
+
+    ga.assocList = angular.module('ahAssocList', ['ui.bootstrap']);
+
+    ga.assocList.controller('assocListController', ['$scope', function ($scope) {
+
+          
+      }]);
+})(window.ahApp);
---
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/AssociateList/service.js
@@ -0,0 +1,17 @@
+﻿(function (ga) {
+    'use strict';
+
+    ga.assocList.factory('listGetService', ['$http', function ($http) {
+        var url = '/workforce-housing-rest/api/associate/';
+        return {
+            get: function (data, pass, fail) {
+                $http({
+                    method: 'get',
+                    url: url,
+                    params: data
+                }).then(pass, fail);
+            }
+        }
+    }]);
+
+})(window.ahApp);
