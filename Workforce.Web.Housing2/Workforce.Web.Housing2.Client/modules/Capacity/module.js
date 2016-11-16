b52d2d0d10d35b3f2c9e3596a04f5ced0e1d159e
danielsmcelhaney <danielsmcelhaney@gmail.com>
2016-11-15 23:44:52 -0500

    graph now going away on redirect as expected TG-35 #in-progress

--- Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Capacity/module.js
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/Capacity/module.js
@@ -103,7 +103,7 @@
               })
 
 
-            var svg = d3.select("body").append("svg")
+            var svg = d3.select("#chart").append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
               .append("g")
