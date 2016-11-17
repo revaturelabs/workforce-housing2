8350975970b883f1a05017cc86a7d158fb27cec2
Alex Roff <alex.roff.career@gmail.com>
2016-11-17 11:43:04 -0500

    Beginner setup for logging.

---
+++ Workforce.Logic.Housing2/Workforce.Logic.Rest/Logger.cs
@@ -0,0 +1,32 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workforce.Logic.Rest
+{
+  public class Logger
+  {
+    private static readonly log4net.ILog log = LogHelper.GetLogger();//log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    public void LoggerMethod()
+    {
+      log.Error("This is an error message");
+    }
+
+    public void InfoLog()
+    {
+      log.Info("This is information");
+    }
+
+    public void WarningLog()
+    {
+      log.Warn("This is a warning message");
+    }
+
+    public void FatalLog()
+    {
+      log.Fatal("This is a fatal log message");
+    }
+  }
+}
