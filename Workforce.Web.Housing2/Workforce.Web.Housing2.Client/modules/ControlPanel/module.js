1b7b53b1b72fad80217001a0fa60f7570b80abea
SmokedEel <msdilinila@gmail.com>
2016-11-18 11:48:37 -0500

    Story/ui overhaul (#31)
    * Removed a button on associates
    
    * Revert "Removed a button on associates"
    
    This reverts commit 4fcfacc565e4bede447561f00443c3514f24fe3b.
    
    * Added new module for control panel
    
    * Addition of control panel
    
    * Slowly getting things to come together
    
    * Slowly piecing the modules together
    
    * Able to move people in an out now.
    
    * Provided changes to module to finish up the overall look
    
    * Removing classes from pages that shouldn't have a specific class
    
    * Replacing changes that were done during merge
    
    * Removed a class from the other modules, removed buttons from control panel. Added control panel to layout nav bar

---
+++ Workforce.Web.Housing2/Workforce.Web.Housing2.Client/modules/ControlPanel/module.js
@@ -0,0 +1,244 @@
+﻿/// <reference path="C:\Revature\1607-jul25-net\1607-workforce-web\Workforce.Web.Grace\Workforce.Web.Grace.Client\Scripts/app.js" />
+
+(function (ga) {
+    'use strict';
+
+    ga.controlPanel = angular.module('ahPanel', ['ui.bootstrap', 'ngMessages']);
+
+    var sessionItem = sessionStorage.getItem('Login');
+    if (sessionItem !== "true") {
+        window.location.href = '#/login';
+    }
+
+    ga.controlPanel.controller('panelController', ['$scope', '$rootScope', '$location', '$window', 'complexGetService', 'complexToAptService',
+    function ($scope, $rootScope, $location, $window, complexGetService, complexToAptService) {
+
+        
+        $scope.filteredComplexes = [];
+        $scope.currentPage = 1;
+        $scope.numPerPage = 10;
+
+        $scope.setPage = function (pageNo) {
+            $scope.currentPage = pageNo;
+        }
+        $scope.pageChanged = function () {
+            var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+            , end = begin + $scope.numPerPage;
+
+            $scope.filteredComplexes = $scope.complexes.slice(begin, end);
+        };
+        
+
+        $scope.get = function () {
+            complexGetService.get(function (response) {
+                var x = $scope.numPerPage;
+                $scope.complexes = response.data;
+                $scope.filteredComplexes = $scope.complexes.slice(0, x);
+                $rootScope.$broadcast('complexObtained', {});
+                
+            })
+        }
+
+        $scope.chosen = function (complex) {
+            complexToAptService.set(complex);
+            $rootScope.$broadcast('complexPicked', {});
+        }
+
+    }]);
+
+    ga.controlPanel.controller('panelAptController', ['$scope', '$rootScope', 'aptGetService', 'filterAptService', 'complexToAptService', 'aptToRoomService', function ($scope, $rootScope, aptGetService, filterAptService, complexToAptService, aptToRoomService) {
+
+        $scope.filteredApartments = [];
+        $scope.aptCurrentPage = 1;
+        $scope.numPerPage = 3;
+
+        
+        $scope.setPage = function (pageNo) {
+            $scope.aptCurrentPage = pageNo;
+        }
+        $scope.aptPageChanged = function () {
+            var begin = (($scope.aptCurrentPage - 1) * $scope.numPerPage)
+            , end = begin + $scope.numPerPage;
+
+            $scope.filteredApartments = $scope.apts.slice(begin, end);
+        };
+
+        $scope.$on('complexObtained', function () {
+            $scope.aptGet();
+        });
+
+        $scope.$on('complexPicked', function () {
+            var x = complexToAptService.get();
+            $('#complexName').html(x.Name);
+            filterAptService.get(x, function (response) {
+                var x = $scope.numPerPage;
+                $scope.apts = response.data;
+                $scope.filteredApartments = $scope.apts.slice(0, x);
+            })
+        });
+
+        $scope.$on('assocMovedIn', function () {
+            var x = complexToAptService.get();
+            filterAptService.get(x, function (response) {
+                var x = $scope.numPerPage;
+                $scope.apts = response.data;
+                $scope.filteredApartments = $scope.apts.slice(0, x);
+            })
+        });
+
+        $scope.$on('assocRemoved', function () {
+            var x = complexToAptService.get();
+            filterAptService.get(x, function (response) {
+                var x = $scope.numPerPage;
+                $scope.apts = response.data;
+                $scope.filteredApartments = $scope.apts.slice(0, x);
+            })
+        });
+    
+        $scope.aptGet = function () {
+            aptGetService.get(function (response) {
+                var x = $scope.numPerPage;
+                $scope.apts = response.data;
+                $scope.filteredApartments = $scope.apts.slice(0, x);
+                $rootScope.$broadcast('apartmentObtained', {});
+            })
+        }
+
+        $scope.aptChoose = function (apt) {
+            aptToRoomService.set(apt);
+            $rootScope.$broadcast('aptPicked', {});
+        }
+    }]);
+
+    ga.controlPanel.controller('panelAssocController', ['$scope', '$rootScope', '$route', 'associateGetService', 'aptToRoomService', 'associatePostService', function ($scope, $rootScope, $route, associateGetService, aptToRoomService, associatePostService) {
+
+        $scope.$on('apartmentObtained', function () {
+            $scope.assocGet();
+        });
+
+        $scope.filteredAssociates = [];
+        $scope.assocCurrentPage = 1;
+        $scope.numPerPage = 5;
+
+        $scope.setPage = function (pageNo) {
+            $scope.assocCurrentPage = pageNo;
+        }
+
+        $scope.assocPageChanged = function () {
+            var begin = (($scope.assocCurrentPage - 1) * $scope.numPerPage)
+            , end = begin + $scope.numPerPage;
+
+            $scope.filteredAssociates = $scope.associates.slice(begin, end);
+        };
+
+        $scope.assocGet = function () {
+            $scope.getModel = {
+                RoomID: -1,
+                AssociateID: -1
+            }
+            associateGetService.get($scope.getModel, function (response) {
+                var x = $scope.numPerPage;
+                $scope.associates = response.data;
+                $scope.filteredAssociates = $scope.associates.slice(0, x);
+            }, function (response) {
+            })
+        }
+
+        
+
+        $scope.moveIn = function (data)
+        {
+            var x = aptToRoomService.get();
+            $scope.moveModel = {
+                RoomID: x.RoomID,
+                AssociateID: data.AssociateID
+            }
+            associatePostService.addAssoc($scope.moveModel, function (result) {
+                aptToRoomService.set(x);
+                $scope.getModel = {
+                    RoomID: -1,
+                    AssociateID: -1
+                }
+                associateGetService.get($scope.getModel, function (response) {
+                    var x = $scope.numPerPage;
+                    $scope.associates = response.data;
+                    $scope.filteredAssociates = $scope.associates.slice(0, x);
+                    $rootScope.$broadcast('assocMovedIn', {});
+                }, function (response) {
+                })
+            });
+        }
+
+        $scope.$on('assocRemoved', function () {
+            $scope.getModel = {
+                RoomID: -1,
+                AssociateID: -1
+            }
+            associateGetService.get($scope.getModel, function (response) {
+                var x = $scope.numPerPage;
+                $scope.associates = response.data;
+                $scope.filteredAssociates = $scope.associates.slice(0, x);
+            }, function (response) {
+            })
+        });
+
+    }]);
+
+    ga.controlPanel.controller('panelRoomController', ['$scope', '$rootScope', '$route', 'aptToRoomService', 'associateByAptService', 'associateDeleteService', function ($scope, $rootScope, $route, aptToRoomService, associateByAptService, associateDeleteService) {
+        
+        $scope.$on('complexPicked', function () {
+            $('#roomNumber').html('Choose an Apartment');
+            $('#assocRoom').empty();
+            $scope.livingAssoc = [];
+
+        });
+
+        $scope.$on('aptPicked', function () {
+            var x = aptToRoomService.get();
+            $('#roomNumber').html('Apt #: ' + x.RoomNumber);
+            associateByAptService.get(x, function (response) {
+                var y = response.data.length;
+                if (y === 0) {
+                    $('#assocRoom').text("There's no one here, would you like to add someone?");
+                }
+                else {
+                    $('#assocRoom').text('');
+                }
+                $scope.livingAssoc = response.data;
+            })
+            
+        });
+
+        $scope.moveOut = function (data) {
+            var y = aptToRoomService.get();
+
+            $scope.deleteModel = {
+                AssociateID: data.AssociateID,
+                RoomID: y.RoomID
+            };
+            associateDeleteService.removeAssoc($scope.deleteModel, function (result) {
+                aptToRoomService.set(y);
+                associateByAptService.get(y, function (response) {
+                    $scope.livingAssoc = response.data;
+                    $rootScope.$broadcast('assocRemoved', {});
+                })
+            });
+        }
+
+        $scope.$on('assocMovedIn', function () {
+            var x = aptToRoomService.get();
+            associateByAptService.get(x, function (response) {
+                var y = response.data.length;
+                if (y === 0) {
+                    $('#assocRoom').text("There's no one here, would you like to add someone?");
+                }
+                else {
+                    $('#assocRoom').text('');
+                }
+                $scope.livingAssoc = response.data;
+            })
+        });
+    }]);
+
+
+})(window.ahApp);
