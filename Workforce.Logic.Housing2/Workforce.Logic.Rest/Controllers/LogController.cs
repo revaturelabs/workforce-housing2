edaae2daabad7d3d11a6ff239246e2d3b6078cf3
Alex Roff <alex.roff.career@gmail.com>
2016-11-22 13:03:31 -0500

    Created minimal lock on log4net, can now read with Log api call in Rest

---
+++ Workforce.Logic.Housing2/Workforce.Logic.Rest/Controllers/LogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.IO;
+
+namespace Workforce.Logic.Rest.Controllers
+{
+  public class LogController : ApiController
+  {
+
+    public async Task<HttpResponseMessage> Get()
+    {
+      var path = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "../HousingLogicLog.log"; //the name of the latest log
+      var reader = new StreamReader(path);
+      string LogInformation = await reader.ReadToEndAsync();
+      reader.Dispose();
+      return Request.CreateResponse(HttpStatusCode.OK, LogInformation);
+    }
+
+  }
+}
+
